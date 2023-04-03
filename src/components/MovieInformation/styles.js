ac4cf82f1a736457ab3a9e6235cf859bf3ab0c07
cdsalty <soltiscd@gmail.com>
2023-04-03 17:15:16 -0400

    updated package.json to include --openssl-legacy-provider to run the application

---
+++ src/components/MovieInformation/styles.js
@@ -0,0 +1,30 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles((theme) => ({
+  containerSpaceAround: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    margin: '10px 0 !important',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+    },
+  },
+  poster: {
+    borderRadius: '20px',
+    boxShadow: '0.5em 1em 1em rgb(64, 64, 70)',
+    width: '80%',
+  },
+  [theme.breakpoints.down('sm')]: {
+    margin: '0 auto',
+    width: '100%',
+    height: '350px',
+    marginBottom: '30px',
+  },
+  [theme.breakpoints.down('md')]: {
+    margin: '0 auto',
+    width: '50%',
+    height: '350px',
+  },
+}));
+src / index.js;
